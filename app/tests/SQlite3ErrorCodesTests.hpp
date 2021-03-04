f4551ae131cf70a588b649c8433564b3e9e895f5
Jakub Opatowski <jakub.opatowski@gmail.com>
2021-03-04 22:47:54 +0100

    Fix for tests

--- app/tests/SQlite3ErrorCodesTests.hpp
+++ app/tests/SQlite3ErrorCodesTests.hpp
@@ -2,6 +2,7 @@
 #include <catch2/catch.hpp>
 
 TEST_CASE( "statusToStr test" ) {
-    REQUIRE( Sqlite3ErrorCodes::statusToStr( 29 ) == "Unknown status" );
-    REQUIRE( Sqlite3ErrorCodes::statusToStr( -1 ) == "Unknown status" );
+    CHECK( std::string( Sqlite3ErrorCodes::statusToStr( 29 ) ) == "Unknown status" );
+    CHECK( std::string( Sqlite3ErrorCodes::statusToStr( -1 ) ) == "Unknown status" );
+    CHECK( std::string( Sqlite3ErrorCodes::statusToStr( 0 ) ) == "Successful result" );
 }
